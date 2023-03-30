4b3f205857e212423aa28d50296a8414e6876fc2
shtuupid <mohamedbarreh8@gmail.com>
2023-03-30 09:39:49 +0100

    changes to books seed

--- seeds/books.js
+++ seeds/books.js
@@ -1,86 +1,49 @@
 const { books } = require('../models');
 
 const booksData = [
-{
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 1,
-    bookname: '',
-    description:
-      '',
+  {
+    id: 1,
+    book_name: 'Harry Potter and Goblet of Fire',
+    availability: true,
+    price: 15.0,
+    filename: 'harry-potter-1.png',
+    user_id: 4,
   },
   {
-    title: '',
-    artist: '',
-    issue_date: '',
-    book_id: 2,
-    bookname: '',
-    description: '',
+    id: 2,
+    book_name: 'Harry Potter and the Philosphers Stone',
+    availability: true,
+    price: 10.0,
+    filename: 'harry-potter-2.png',
+    user_id: 2,
   },
   {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 3,
-    bookname: '',
-    description: '',
+    id: 3,
+    book_name: 'Harry Potter and the Prisoner of Azkaban',
+    availability: false,
+    price: 14.0,
+    filename: 'harry-potter-3.png',
+    user_id: 1,
   },
   {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 4,
-    bookname: '',
-    description: '',
+    id: 4,
+    book_name: 'Harry Potter and the Order of the Phoenix',
+    availability: true,
+    price: 13.0,
+    filename: 'harry-potter-4.png',
+    user_id: 5,
   },
   {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 5,
-    bookname: '',
-    description: '',
-  },
-  {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 6,
-    bookname: '',
-    description:
-      '',
-  },
-  {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 7,
-    bookname: '',
-    description:
-      '',
-  },
-  {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 8,
-    bookname: '',
-    description:
-      '',
-  },
-  {
-    title: '',
-    author: '',
-    issue_date: '',
-    book_id: 9,
-    bookname: '',
-    description:
-      '',
+    id: 5,
+    book_name: 'Harry Potter and the Half-Blood Prince',
+    availability: true,
+    price: 11.0,
+    filename: 'harry-potter-5.png',
+    user_id: 3,
   },
 ];
 
-const seedBooks = () => Books.bulkCreate(booksdata);
+const seedBooks = () => Books.bulkCreate(booksData);
 
 module.exports = seedBooks;
 
