a6c1df809fb28f062aac4f5b67f1cb8cb61d1d98
shtuupid <mohamedbarreh8@gmail.com>
2023-04-01 13:29:28 +0100

    edits to homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,20 @@ const { Books, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const dbBooksData = await Books.findAll({});
+    const dbBooksData = await Books.findAll({
+      include: [
+        {
+          model: Books,
+          attributes: ['filename', 'book_name'],
+        },
+      ],
+    });
     const books = dbBooksData.map((books) =>
       books.get({ plain: true })
     );
-    res.render("home", {
+    res.render("homepage", {
       books,
+      loggedIn: req.session.loggedIn,
     });
   } 
   catch (err) {
