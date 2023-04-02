becca9f0ae1f5428508b9b0e415f1180612c6b48
shtuupid <mohamedbarreh8@gmail.com>
2023-04-02 10:55:03 +0100

    additions to home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -24,4 +24,37 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get('/books/:id', withAuth, async (req, res) => {
+  try {
+    const dbBooksData = await books.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+    const books = dbBooksData.get({ plain: true });
+    res.render('books', { books, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 module.exports = router;
