02678e63d423935e39635b918bc2e263507addb5
shtuupid <mohamedbarreh8@gmail.com>
2023-04-06 18:55:12 +0100

    final

--- public/js/send.js
+++ public/js/send.js
@@ -2,11 +2,10 @@ const sendHandler = async (event) => {
   const name = document.querySelector('#name').value;
   const description = document.querySelector('#description').value;
   const price = document.querySelector('#price').value;
-
     event.preventDefault();
       const response = await fetch(`/api/books/send`, {
         method: 'POST',
-        body: JSON.stringify({name, description,price}),
+        body: JSON.stringify({name, description, price}),
         headers: { 'Content-Type': 'application/json' },
         });
 
